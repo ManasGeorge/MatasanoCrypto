57bd223584d1db28dccc0520f1a1938a4963dcc4
Manas George <manas3george@gmail.com>
2014-08-14 19:37:38 +0400

    Package into function

--- Set 1/challenge1.py
+++ Set 1/challenge1.py
@@ -1,9 +1,14 @@
 import binascii
 import base64
 
-h = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-s = binascii.unhexlify(h)
-
-print base64.b64encode(s)
-
 # The hex string represents an ASCII string, which is what must be converted to Base64
+
+def hextob64(h):
+	return base64.b64encode(binascii.unhexlify(h))
+
+def main():
+	h = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	print hextob64(h)
+
+if __name__ == "__main__":
+	main()
